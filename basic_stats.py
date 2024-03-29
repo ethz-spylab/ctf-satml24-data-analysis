935366173b4afa2051897f93c5296645f2622776
Daniel Paleka <danepale@gmail.com>
2024-03-30 00:24:22 +0100

    Refactor defense dictionary creation for team and model; fix bug with indexing

--- basic_stats.py
+++ basic_stats.py
@@ -1,39 +1,45 @@
 #%%
 import json
-from pathlib import Path
+from tqdm import tqdm
 
-PATH = Path("/data/projects/satml-llm-ctf-analysis/ctf-satml24")
+from utils import PATH, valid_defense, defense_lookup, short_model
 
+print(defense_lookup)
+
+#%%
 # Load chats.json
 with open(PATH / 'chat.json', 'r') as chats_file:
     chats = [json.loads(line) for line in chats_file]
 
-# Load valid_defense.json
-with open(PATH / 'valid_defense.json', 'r') as valid_defense_file:
-    valid_defense = [json.loads(line) for line in valid_defense_file]
 
 #%%
-# Create a dictionary of defenses wrt defense id
-defenses = {defense["defense_data"]["defense_id"]: defense for defense in valid_defense}
-attacks = {defense["defense_data"]["defense_id"]: [] for defense in valid_defense}
+# Create a dictionary of defenses wrt (team, model) id
+defenses = {(defense["team"], short_model[defense["model"]]): defense for defense in valid_defense}
+attacks = {(defense["team"], short_model[defense["model"]]): [] for defense in valid_defense}
 
 # Construct attacks dict from chats
 for chat in chats:
     chat_defense_id = chat["defense"]["$id"]["$oid"]
+    chat_model = chat["model"]
+    chat_team, submission_id = defense_lookup[(chat_model, chat_defense_id)]
+
     if chat["is_attack"]:
         try:
-            attacks[chat_defense_id].append(chat)
+            attacks[(chat_team, short_model[chat_model])].append(chat)
         except KeyError:
             print(f"Defense {chat_defense_id} not found in defenses")
 
 
 #%%
+print(len(defenses))
+print(len(attacks[(chat_team, short_model[chat_model])]))
+#%%
 # Count the number of attacks for each defense
-attack_counts = {defense_id: len(attacks[defense_id]) for defense_id in attacks}
+attack_counts = {(team, model): len(attacks[(team, model)]) for (team, model) in attacks}
 
 # Print the number of attacks for each defense
-for defense_id, count in attack_counts.items():
-    print(f"{defenses[defense_id]['team']}, {defenses[defense_id]['model']}: {count} attacks")
+for (team, model), count in attack_counts.items():
+    print(f"{team}, {model}: {count} attacks")
 
 
 # %%
@@ -43,17 +49,21 @@ for defense_id, count in attack_counts.items():
 import pandas as pd
 
 # Create a DataFrame to store the message counts
-msg_counts = pd.DataFrame(0, index=defenses.keys(), columns=list(range(0, 16)) + ["more"])
+buckets = [[2], [4], [6], [8,10,12,14], list(range(16, 1000, 2)), [0]]
+bucket_names = ["2", "4", "6", "8-14", "more", "empty"]
+msg_counts = pd.DataFrame(0, index=defenses.keys(), columns=bucket_names)
+
 
 # Fill the DataFrame with the message counts
-for defense_id, attack_list in attacks.items():
+for (team, model), attack_list in tqdm(attacks.items()):
     for attack in attack_list:
         msgs = attack["history"]
-        print(len(msgs))
-        if len(msgs) > 15:
-            msg_counts.loc[defense_id, "more"] += 1
+        for i, bucket in enumerate(buckets):
+            if len(msgs) in bucket:
+                msg_counts.loc[(team, model), bucket_names[i]] += 1
+                break
         else:
-            msg_counts.loc[defense_id, len(msgs)] += 1
+            print(f"{team}, {model}: {len(msgs)} messages")
 
 # Print the DataFrame
 print(msg_counts)
---
+++ utils.py
@@ -0,0 +1,16 @@
+import json
+from pathlib import Path
+PATH = Path("/data/projects/satml-llm-ctf-analysis/ctf-satml24")
+
+# Load valid_defense.json
+with open(PATH / 'valid_defense.json', 'r') as valid_defense_file:
+    valid_defense = [json.loads(line) for line in valid_defense_file]
+
+#%%
+short_model = {
+    "openai/gpt-3.5-turbo-1106": "gpt35",
+    "meta/llama-2-70b-chat": "llama"
+}
+
+# Create a dictionary to lookup defense
+defense_lookup = {(defense["model"], defense["defense_data"]["defense_id"]): (defense["team"], defense["submission_id"]) for defense in valid_defense}
