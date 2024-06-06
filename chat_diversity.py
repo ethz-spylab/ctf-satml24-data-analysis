3151e8c993c23bed6c5b12a23a1860d8c7f13cb9
Daniel Paleka <danepale@gmail.com>
2024-06-06 17:01:24 +0200

    t(attacker team, defense) pairs

--- chat_diversity.py
+++ chat_diversity.py
@@ -43,7 +43,7 @@ def count_unique_prefixes(filename, prefix_length=PREFIX_LENGTH, is_jsonl=False,
             'unique': len(unique_prefixes)}
 
 #%%
-def count_unique_attacker_defender_pairs(filename, is_jsonl=False, only_successful=False):
+def count_unique_attacker_defender_pairs(filename, is_jsonl=False, only_successful=False, only_unsuccessful=False):
     with open(filename, 'r') as file:
         if is_jsonl:
             chats = []
@@ -54,8 +54,11 @@ def count_unique_attacker_defender_pairs(filename, is_jsonl=False, only_successf
     
     chats = [chat for chat in chats if chat['history'] and chat['history'][0]['content'] is not None]
 
+    assert not (only_successful and only_unsuccessful)
     if only_successful:
         chats = [chat for chat in chats if chat['was_successful_secret_extraction']]
+    elif only_unsuccessful:
+        chats = [chat for chat in chats if not chat['was_successful_secret_extraction']]
     
     attacker_defender_pairs = set()
     attackers = set()
@@ -69,6 +72,41 @@ def count_unique_attacker_defender_pairs(filename, is_jsonl=False, only_successf
     
     return {'total': len(chats),
             'unique': len(attacker_defender_pairs),
+            'unsuccessful': len(chats) - len(attacker_defender_pairs),
+            'attackers': len(attackers),
+            'defenders': len(defenders)}
+
+
+def count_unique_attacker_defender_teams(filename, is_jsonl=False, only_successful=False, only_unsuccessful=False):
+    with open(filename, 'r') as file:
+        if is_jsonl:
+            chats = []
+            for line in tqdm(file):
+                chats.append(json.loads(line))
+        else:
+            chats = json.load(file)
+    
+    chats = [chat for chat in chats if chat['history'] and chat['history'][0]['content'] is not None]
+
+    assert not (only_successful and only_unsuccessful)
+    if only_successful:
+        chats = [chat for chat in chats if chat['was_successful_secret_extraction']]
+    elif only_unsuccessful:
+        chats = [chat for chat in chats if not chat['was_successful_secret_extraction']]
+    
+    attacker_defender_pairs = set()
+    attackers = set()
+    defenders = set()
+    for chat in tqdm(chats):
+        attacker = chat['user']['team']
+        defense = chat['defense']['team'] + "," + chat['model']
+        attackers.add(attacker)
+        defenders.add(defense)
+        attacker_defender_pairs.add((attacker, defense))
+    
+    return {'total': len(chats),
+            'unique': len(attacker_defender_pairs),
+            'unsuccessful': len(chats) - len(attacker_defender_pairs),
             'attackers': len(attackers),
             'defenders': len(defenders)}
 
@@ -76,7 +114,7 @@ def count_unique_attacker_defender_pairs(filename, is_jsonl=False, only_successf
 dataset_path = PATH
 
 test_filename = f"{dataset_path}/chat.120.json"
-main_filename = f"{dataset_path}/filtered_chat.json"
+main_filename = f"{dataset_path}/chat.json"
 #%%
 # Run the attacker-defender function on the test file first
 test_data = count_unique_attacker_defender_pairs(test_filename)
@@ -99,6 +137,33 @@ main_data = count_unique_attacker_defender_pairs(main_filename, is_jsonl=True, o
 print(f"Number of unique successful attacker-defender pairs in main file: {main_data['unique']} out of {main_data['total']}")
 print(f"Unique attackers: {main_data['attackers']}, unique defenders: {main_data['defenders']}")
 
+main_data = count_unique_attacker_defender_pairs(main_filename, is_jsonl=True, only_unsuccessful=True)
+print(f"Number of unique unsuccessful attacker-defender pairs in main file: {main_data['unique']} out of {main_data['total']}")
+print(f"Unique attackers: {main_data['attackers']}, unique defenders: {main_data['defenders']}")
+
+#%%
+# The same for teams
+test_data = count_unique_attacker_defender_teams(test_filename)
+print(f"Number of unique attacker-defender team pairs in test file: {test_data['unique']} out of {test_data['total']}")
+print(f"Unique attackers: {test_data['attackers']}, unique defenders: {test_data['defenders']}")
+
+test_data = count_unique_attacker_defender_teams(test_filename, only_successful=True)
+print(f"Number of unique successful attacker-defender team pairs in test file: {test_data['unique']} out of {test_data['total']}")
+print(f"Unique attackers: {test_data['attackers']}, unique defenders: {test_data['defenders']}")
+
+#%%
+# Now run the teams sattacker-defender function on the main file
+main_data = count_unique_attacker_defender_teams(main_filename, is_jsonl=True)
+print(f"Number of unique attacker-defender team pairs in main file: {main_data['unique']} out of {main_data['total']}")
+print(f"Unique attackers: {main_data['attackers']}, unique defenders: {main_data['defenders']}")
+
+main_data = count_unique_attacker_defender_teams(main_filename, is_jsonl=True, only_successful=True)
+print(f"Number of unique successful attacker-defender team pairs in main file: {main_data['unique']} out of {main_data['total']}")
+print(f"Unique attackers: {main_data['attackers']}, unique defenders: {main_data['defenders']}")
+
+main_data = count_unique_attacker_defender_teams(main_filename, is_jsonl=True, only_unsuccessful=True)
+print(f"Number of unique successful attacker-defender team pairs in main file: {main_data['unique']} out of {main_data['total']}")
+print(f"Unique attackers: {main_data['attackers']}, unique defenders: {main_data['defenders']}")
 
 #%%
 # Run the function on the test file first
